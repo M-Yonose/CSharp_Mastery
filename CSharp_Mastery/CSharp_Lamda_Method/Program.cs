11613acdc3ed17ba5b9ee7a146a3b18eee3e89a2
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-18 23:17:39 +0600

    pushing previous session code files

--- CSharp_Mastery/CSharp_Lamda_Method/Program.cs
+++ CSharp_Mastery/CSharp_Lamda_Method/Program.cs
@@ -1,16 +1,4 @@
-﻿//int Sum(int a, int b) => a + b;
-
-//void Print(object s) => Console.WriteLine(s);
-
-
-//var sum = Sum(3, 7);
-//Print(sum);
-
-
-//// Anonymous method
-//var m = (double a, double b) => a * b;
-
-//double x = m(3, 6);
+﻿//Lamda method
 
 int Sum(int a, int b) => a + b;
 void print(object s) => Console.WriteLine(s);
@@ -19,6 +7,7 @@ var sum = Sum(3, 7);
 print(sum);
 
 //Anonymous method
+
 var m = (double a, double b) => a * b;
 double x = m(3, 6);
 print(x);
