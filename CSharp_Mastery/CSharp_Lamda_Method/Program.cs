db6ba053ef48e60ae75c74c8aa02e7fcf8df70ae
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-18 17:58:58 +0600

    Practice Lambda method and Anonymous method inCSharp

---
+++ CSharp_Mastery/CSharp_Lamda_Method/Program.cs
@@ -0,0 +1,24 @@
+﻿//int Sum(int a, int b) => a + b;
+
+//void Print(object s) => Console.WriteLine(s);
+
+
+//var sum = Sum(3, 7);
+//Print(sum);
+
+
+//// Anonymous method
+//var m = (double a, double b) => a * b;
+
+//double x = m(3, 6);
+
+int Sum(int a, int b) => a + b;
+void print(object s) => Console.WriteLine(s);
+
+var sum = Sum(3, 7);
+print(sum);
+
+//Anonymous method
+var m = (double a, double b) => a * b;
+double x = m(3, 6);
+print(x);
