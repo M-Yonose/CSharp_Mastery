92d69e1392ef1e05783b5b6bbe9b78320e0f33eb
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-18 16:05:41 +0600

    Practice CSharp loops and operators with example and asp.net class_4 code repeat

---
+++ CSharp_Mastery/CSharp_Loops/Program.cs
@@ -0,0 +1,48 @@
+﻿//For loop
+for(int i = 0; i <5; i++)
+{
+    Console.WriteLine(i);
+}
+
+//Example of a for loop: print Even numbers
+for(int i = 2; i <- 10; i += 2)
+{
+    Console.WriteLine(i);
+}
+
+//While loop
+int w = 1;
+
+while(w <= 5)
+{
+    Console.WriteLine(w);
+    w++;
+}
+//Do While loop
+int X = 10;
+
+do
+{
+    Console.WriteLine(X);
+    X--;
+}
+while(X > 10);
+
+//For Each loop
+int[] numbers = { 11, 22, 33, 44, 55 };
+foreach( int values in numbers)
+{
+    Console.WriteLine(values);
+}
+
+//Nested Loops
+
+for (int i = 1; i <= 3; i++)
+{
+    for (int j = 1; j <= 3; j++)
+    {
+        Console.Write($"({i}, {j}) ");
+    }
+
+    Console.WriteLine();
+}
---
+++ CSharp_Mastery/CSharp_Operatores/Program.cs
@@ -0,0 +1,124 @@
+﻿//Conditional (Ternary) Operator
+int age = 20;
+string result = age >=18 ? "adult" : "minor";
+Console.WriteLine(result);
+
+//Null-Coalescing Operator
+string? name = null;
+string Username = name ?? "Guest";
+Console.WriteLine(Username);
+
+///A real life example
+int english = 80;
+int science = 70;
+int physics = 87;
+
+double avg = (english + science + physics) / 3.0;
+
+bool passed = avg >= 50;
+
+Console.WriteLine($"Avarage of three subjects: {avg}");
+Console.WriteLine($"Passed: {passed}");
+
+////////Practice from ASP.NET course class_4 codes:
+
+int x = 5;
+
+x++; // x = x + 1;
+++x; // x = x + 1;
+
+x--; // x = x - 1;
+--x; // x = x - 1;
+
+
+int a = 5;
+int z = a++;
+Console.WriteLine("a:" + a);
+Console.WriteLine("z:" + z);
+
+
+int b = 5;
+int w = ++b;
+Console.WriteLine("b:" + b);
+Console.WriteLine("w:" + w);
+
+
+int c = a + b;
+int c2 = a - b;
+int c3 = a * b;
+int c4 = a / b;
+int c5 = a % b;
+
+int t = (a * b - c) / x++;
+
+
+int heightOfRahim = 45;
+int heightOfKarim = 50;
+
+bool isRahimTaller = heightOfRahim > heightOfKarim;
+bool isKarimTaller = heightOfRahim < heightOfKarim;
+bool isEqualHeight = heightOfRahim == heightOfKarim;
+bool isNotEqualHeight = heightOfRahim != heightOfKarim;
+bool isRahimTallerOrEqual = heightOfRahim >= heightOfKarim;
+bool isKarimTallerOrEqual = heightOfRahim <= heightOfKarim;
+
+bool isRahimTallerAndTall = heightOfRahim > heightOfKarim && heightOfRahim > 40;
+bool isRahimTallerOrEqualAndTall = heightOfRahim < heightOfKarim || heightOfRahim > 40;
+
+/*
+ *  true && true = true
+ *  true && false = false
+ *  false && false = false
+ *  
+ *  true || true = true
+ *  true || false = true
+ *  false || false = false
+ * 
+ **/
+
+
+
+
+// 0-1 -> Computer, Binary
+// 0-9 -> Human, Decimal
+// 0-7 -> Octal
+// 0-9, A-F -> Hexadecimal
+
+int p = 933_048_309;
+
+int[] q = [3, 4, 9, 4, 3, 7];
+
+int r = 3 * 10 ^ 5 + 4 * 10 ^ 4 + 9 * 10 ^ 3 + 4 * 10 ^ 2 + 3 * 10 ^ 1 + 7 * 10 ^ 0;
+int r2 = 0b_1101; // 1 * 2^3 + 1 * 2^2 + 0 * 2^1 + 1 * 2^0
+
+Console.WriteLine("r2:" + r2);
+Console.WriteLine("r2 in binary: " + Convert.ToString(r2, toBase: 2));
+
+Console.WriteLine("p in binary: " + Convert.ToString(p, toBase: 2));
+
+
+// 1010111001 -> 0010101110
+
+// 1010111001 <- 1011100100
+
+int m1 = 0b_1010111001;
+m1 = m1 << 2; // 101011100100
+Console.WriteLine("m1 in binary: " + Convert.ToString(m1, toBase: 2));
+
+int m2 = 0b_1010111001;
+m2 = m2 >> 2; // 0010101110
+Console.WriteLine("m2 in binary: " + Convert.ToString(m2, toBase: 2));
+
+int n1 = 0b_1010111001;
+int n2 = 0b_0010101110;
+
+int t1 = n1 & n2; // 0010101000
+int t2 = n1 | n2; // 1010111111
+Console.WriteLine("t1 in binary: " + Convert.ToString(t1, toBase: 2));
+Console.WriteLine("t2 in binary: " + Convert.ToString(t2, toBase: 2));
+
+int u = 31;
+
+Console.WriteLine(Convert.ToString(u, 2));
+Console.WriteLine(Convert.ToString(u, 8));
+Console.WriteLine(Convert.ToString(u, 16));
